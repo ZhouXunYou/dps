93b5e93a4ac04b50521f0877f42da68a7a6a225a
zhangmingyong <zhangmingyong@dcits.com>
2020-06-08 11:17:35 +0800

    接入kafka

--- dps-datasource/src/test/scala/samples/junit.scala
+++ dps-datasource/src/test/scala/samples/junit.scala
@@ -2,16 +2,48 @@ package samples
 
 import org.junit._
 import Assert._
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.{Seconds,StreamingContext,kafka010}
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
 
 @Test
 class AppTest {
 
-    @Test
-    def testOK() = assertTrue(true)
+//  @Test
+//  def testOK() = assertTrue(true)
 
-//    @Test
-//    def testKO() = assertTrue(false)
+  //    @Test
+  //    def testKO() = assertTrue(false)
+
+  @Test
+  def kafka() {
+    val sparkConf: SparkConf = new SparkConf().setAppName("Kafka_Receiver").setMaster("local[*]")
+    val sc = new SparkContext(sparkConf)
+    val ssc = new StreamingContext(sc,Seconds(5))
+    
+    val kafkaParams=Map[String,Object](
+      "bootstrap.servers"->"192.168.11.200:9092",
+      "key.deserializer"->classOf[StringDeserializer],
+      "value.deserilazier"->classOf[StringDeserializer],
+      "group.id"->"groupName",
+      "auto.offset.reset"->"latest",
+      "enable.auto.commit"->(false:java.lang.Boolean)
+      )
+    val topics=List("logstash_test")
+    val lines=KafkaUtils.createDirectStream[String,String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String,String](topics,kafkaParams)
+    )
+    
+    lines.print()
+    
+    ssc.start()
+    ssc.awaitTermination()
+  }
 
 }
-
-
