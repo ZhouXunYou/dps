1b3680f778070f0e6a62e1fc055ad3dde1282f93
ZhouXunYou <zhouxy@zctt.com>
2020-06-09 18:21:51 +0800

    初始化数据源数据更新

--- dps-datasource/src/main/scala/dps/datasource/util/DatasourceUtils.scala
+++ dps-datasource/src/main/scala/dps/datasource/util/DatasourceUtils.scala
@@ -9,31 +9,37 @@ import org.apache.spark.SparkContext
 import dps.utils.SessionOperation
 import dps.datasource.DataSource
 import dps.datasource.StreamDatasource
+import java.lang.reflect.Modifier
+import dps.atomic.Operator
+import org.apache.spark.SparkConf
 
 object DatasourceUtils {
   private val packageName = "dps.datasource"
   def main(args: Array[String]): Unit = {
-    //    val so = new SessionOperation("org.postgresql.Driver", "jdbc:postgresql://39.98.141.108:16632/dps", "postgres", "1qaz#EDC")
-//    val so = new SessionOperation("org.postgresql.Driver", "jdbc:postgresql://10.1.1.99:5432/dps", "postgres", "postgres")
-    
-//    val so = new SessionOperation("com.mysql.jdbc.Driver", "jdbc:mysql://39.98.141.108:16606/dps?useUnicode=true&characterEncoding=UTF-8&useSSL=false", "root", "1qaz#EDC")
-    val so = new SessionOperation("com.mysql.jdbc.Driver", "39.98.141.108","16606", "root", "1qaz#EDC","mysql","dps")
+    val so = new SessionOperation("org.postgresql.Driver", "192.168.36.240", "3306", "root", "", "mysql", "dps")
+
+    //    val so = new SessionOperation("com.mysql.jdbc.Driver", "39.98.141.108", "16606", "root", "1qaz#EDC", "mysql", "dps")
     so.executeUpdate("truncate table s_datasource_param_define", Array())
     so.executeUpdate("truncate table s_datasource_define", Array())
-    getDatasources().foreach(datasource=>{
-      val datasourceInstance = datasource.getConstructor(classOf[SparkContext],classOf[Map[String,String]]).newInstance(null,Map[String,String]()).asInstanceOf[DataSource]
-      initDatasource(datasourceInstance,so)
+    val sparkConf = new SparkConf()
+    sparkConf.setAppName("initDatasourceData")
+    sparkConf.setMaster("local[*]")
+    val sparkContext = new SparkContext(sparkConf)
+    getDatasources().foreach(datasource => {
+      println(datasource.getName)
+      val datasourceInstance = datasource.getConstructor(classOf[SparkContext], classOf[Map[String, String]],classOf[Operator]).newInstance(sparkContext, Map[String, String](),null).asInstanceOf[DataSource]
+      initDatasource(datasourceInstance, so)
     })
   }
-  def initDatasource(datasource:DataSource,so:SessionOperation){
+  def initDatasource(datasource: DataSource, so: SessionOperation) {
     val define = datasource.define()
-    val datasourceParams = Array[Any](define.id,define.datasourceName,datasource.getClass.getName)
+    val datasourceParams = Array[Any](define.id, define.datasourceName, datasource.getClass.getName)
     so.executeUpdate("insert into s_datasource_define(id,datasource_name,datasource_class) values (?,?,?)", datasourceParams)
-    define.datasourceDefinParams.foreach(datasourceDefinParam=>{
+    define.datasourceDefinParams.foreach(datasourceDefinParam => {
       val datasourceParamCode = datasourceDefinParam._1
       val datasourceParamDefine = datasourceDefinParam._2
       val id = s"${define.id}_${datasourceParamCode}"
-      val params = Array[Any](id,define.id,datasourceParamCode,datasourceParamDefine.paramName,datasourceParamDefine.defaultValue)
+      val params = Array[Any](id, define.id, datasourceParamCode, datasourceParamDefine.paramName, datasourceParamDefine.defaultValue)
       so.executeUpdate("insert into s_datasource_param_define(id,datasource_id,datasource_param_code,datasource_param_name,datasource_param_default_value) values(?,?,?,?,?)", params)
     })
   }
@@ -41,11 +47,14 @@ object DatasourceUtils {
     var datasources = List[Class[_]]()
     val url = DatasourceUtils.getClass.getClassLoader.getResource(packageName.replace(".", "/"))
     val sourcePackage = new File(url.getFile)
-    sourcePackage.listFiles().foreach(file=>{
-      if(file.isFile() && file.getName.endsWith(".class")){
-        val clazz = Class.forName(Array(packageName,file.getName.split("\\.").apply(0)).mkString("."))
-        if(clazz.getSuperclass == classOf[DataSource]  || clazz.getSuperclass == classOf[StreamDatasource]){
-          datasources = clazz :: datasources
+    val fiels = sourcePackage.listFiles();
+    sourcePackage.listFiles().foreach(file => {
+      if (file.isFile() && file.getName.endsWith(".class")) {
+        val clazz = Class.forName(Array(packageName, file.getName.split("\\.").apply(0)).mkString("."))
+        if (!Modifier.isAbstract(clazz.getModifiers)) {
+          if (clazz.getSuperclass == classOf[DataSource] || clazz.getSuperclass == classOf[StreamDatasource]) {
+            datasources = clazz :: datasources
+          }
         }
       }
     })
