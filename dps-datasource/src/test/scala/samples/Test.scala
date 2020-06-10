c55c3aa842c07a126b2ae4b28b19495c376e5db2
zhangmingyong <zhangmingyong@dcits.com>
2020-06-10 21:45:29 +0800

    更新数据源与原子操作传参sparkContext为sparkSession

--- dps-datasource/src/test/scala/samples/Test.scala
+++ dps-datasource/src/test/scala/samples/Test.scala
@@ -1,38 +1,31 @@
 package samples
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
-
-import dps.datasource.DataSource
-import dps.datasource.StreamDatasource
 import dps.atomic.Operator
 import dps.atomic.model.OperationGroup
-import scala.collection.mutable.Map
+import dps.datasource.{DataSource, StreamDatasource}
+import org.apache.spark.sql.SparkSession
+
 import scala.collection.immutable.List
+import scala.collection.mutable.Map
 
 object Test {
   def main(args: Array[String]): Unit = {
-    val sparkConf: SparkConf = new SparkConf().setAppName("Kafka_Receiver").setMaster("local[*]")
-    val sc = new SparkContext(sparkConf)
-    import scala.collection.mutable.Map
+    val sparkSession = SparkSession.builder().appName("Kafka_Receiver").master("local[*]").getOrCreate()
     val map = Map[String, String]()
-//    map.put("duration", "10")
+    map.put("duration", "10")
     map.put("bootstrapServers", "192.168.11.200:9092")
     map.put("group", "groupTest")
     map.put("topics", "logstash_test")
-    
-    val operationGroups:List[OperationGroup]=null
-    val missionVariables:Map[String, Any]=Map[String,Any]()
-    val o = new Operator(operationGroups,sc,missionVariables)
+
+    val operationGroups: List[OperationGroup] = null
+    val missionVariables: Map[String, Any] = Map[String, Any]()
+    val o = new Operator(operationGroups, sparkSession, missionVariables)
     val s2 = Class.forName("dps.datasource.KafkaSource")
-      .getConstructor(classOf[SparkContext], classOf[Map[String, String]], classOf[Operator])
-      .newInstance(sc, map,o)
+      .getConstructor(classOf[SparkSession], classOf[Map[String, String]], classOf[Operator])
+      .newInstance(sparkSession, map, o)
       .asInstanceOf[DataSource]
     val rdd = s2.read("a");
-//    rdd.asInstanceOf[RDD[String]].foreach(f=>{
-//      println(f)
-//    })
     s2.asInstanceOf[StreamDatasource].start()
   }
+
 }
--- dps-datasource/src/test/scala/samples/junit.scala
+++ dps-datasource/src/test/scala/samples/junit.scala
@@ -1,22 +1,20 @@
 package samples
 
-import org.junit._
-import Assert._
+import dps.datasource.{DataSource, StreamDatasource}
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.streaming.{Seconds,StreamingContext,kafka010}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.KafkaUtils
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import dps.datasource.StreamDatasource
-import dps.datasource.DataSource
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.junit._
 
 @Test
 class AppTest {
 
-//  @Test
-//  def testOK() = assertTrue(true)
+  //  @Test
+  //  def testOK() = assertTrue(true)
 
   //    @Test
   //    def testKO() = assertTrue(false)
@@ -26,57 +24,58 @@ class AppTest {
     val sparkConf: SparkConf = new SparkConf().setAppName("Kafka_Receiver").setMaster("local[*]")
     sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     val sc = new SparkContext(sparkConf)
-    val ssc = new StreamingContext(sc,Seconds(5))
-    
-    val kafkaParams=Map[String,Object](
-      "bootstrap.servers"->"192.168.11.200:9092",
-      "key.deserializer"->classOf[StringDeserializer],
-      "value.deserializer"->classOf[StringDeserializer],
-      "group.id"->"groupName",
-      "auto.offset.reset"->"latest",
-      "enable.auto.commit"->(true:java.lang.Boolean)
-      )
-    val topics=List("logstash_test")
-    val lines=KafkaUtils.createDirectStream[String,String](
+    val ssc = new StreamingContext(sc, Seconds(5))
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "192.168.11.200:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "groupName",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (true: java.lang.Boolean)
+    )
+    val topics = List("logstash_test")
+    val lines = KafkaUtils.createDirectStream[String, String](
       ssc,
       PreferConsistent,
-      Subscribe[String,String](topics,kafkaParams)
+      Subscribe[String, String](topics, kafkaParams)
     )
-    
-    lines.foreachRDD(rdd=>{
-      rdd.map(record=>{
+
+    lines.foreachRDD(rdd => {
+      rdd.map(record => {
         val array = record.value().split(",")
-        Map[String,String](array.apply(0)->array.apply(1))
-      }).foreach(r=>{
+        Map[String, String](array.apply(0) -> array.apply(1))
+      }).foreach(r => {
         println(r)
       })
     })
-    
+
     ssc.start()
     ssc.awaitTermination()
   }
+
   @Test
   def typeTest() {
-    val sparkConf: SparkConf = new SparkConf().setAppName("Kafka_Receiver").setMaster("local[*]")
-    val sc = new SparkContext(sparkConf)
+    val sparkSession = SparkSession.builder().appName("Kafka_Receiver").master("local[*]").getOrCreate()
     import scala.collection.mutable.Map
     val map = Map[String, String]();
     map.put("duration", "555555");
     val s1 = Class.forName("dps.datasource.FileSource")
-      .getConstructor(classOf[SparkContext], classOf[Map[String, String]])
-      .newInstance(sc, map)
+      .getConstructor(classOf[SparkSession], classOf[Map[String, String]])
+      .newInstance(sparkSession, map)
       .asInstanceOf[DataSource]
-    
+
     val s2 = Class.forName("dps.datasource.KafkaSource")
-      .getConstructor(classOf[SparkContext], classOf[Map[String, String]])
-      .newInstance(sc, map)
+      .getConstructor(classOf[SparkSession], classOf[Map[String, String]])
+      .newInstance(sparkSession, map)
       .asInstanceOf[DataSource]
-    
+
     println(s1.isInstanceOf[DataSource])
     println(s2.isInstanceOf[StreamDatasource])
-   
+
   }
-  def numTest(){
-    
+
+  def numTest() {
+
   }
 }
