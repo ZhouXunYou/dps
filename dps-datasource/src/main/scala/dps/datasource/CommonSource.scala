7bb87e87bf5fed374f396750c20e1dc81f4b7880
ZhouXunYou <zhouxy@zctt.com>
2020-07-16 17:11:38 +0800

    去掉scope的设置

--- dps-datasource/src/main/scala/dps/datasource/CommonSource.scala
+++ dps-datasource/src/main/scala/dps/datasource/CommonSource.scala
@@ -9,15 +9,16 @@ import dps.datasource.define.DatasourceDefine
 import dps.datasource.define.DatasourceParamDefine
 import org.apache.spark.SparkConf
 
-class CommonSource(override val sparkSession: SparkSession, override val sparkConf: SparkConf, override val params: Map[String, String], override val operator: Operator) extends DataSource(sparkSession, sparkConf, params, operator) {
-  def read(variableKey: String) {
-    operator.operation();
-  }
-  def define(): DatasourceDefine = {
-    val paramDefines = Map[String, DatasourceParamDefine](
-      "configParam" -> new DatasourceParamDefine("configParam", "{}"))
-    val datasourceDefine = new DatasourceDefine("Common", paramDefines.toMap)
-    datasourceDefine.id = "common_source_define"
-    return datasourceDefine
-  }
+class CommonSource{
+//(override val sparkSession: SparkSession, override val sparkConf: SparkConf, override val params: Map[String, String], override val operator: Operator) extends DataSource(sparkSession, sparkConf, params, operator) {
+//  def read(variableKey: String) {
+//    operator.operation();
+//  }
+//  def define(): DatasourceDefine = {
+//    val paramDefines = Map[String, DatasourceParamDefine](
+//      "configParam" -> new DatasourceParamDefine("configParam", "{}"))
+//    val datasourceDefine = new DatasourceDefine("Common", paramDefines.toMap)
+//    datasourceDefine.id = "common_source_define"
+//    return datasourceDefine
+//  }
 }
