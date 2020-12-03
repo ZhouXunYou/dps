f83a22179910fbf5a30aa8398846f5c4b61cfc38
周训游 <zhouxunyou@gmail.com>
2020-12-03 14:48:40 +0800

    nothing

--- dps-atomic/src/main/scala/dps/atomic/impl/ml/Correlation.scala
+++ dps-atomic/src/main/scala/dps/atomic/impl/ml/Correlation.scala
@@ -17,9 +17,7 @@ import dps.atomic.impl.AbstractAction
 class Correlation(override val sparkSession: SparkSession, override val sparkConf: SparkConf, override val inputVariableKey: String, override val outputVariableKey: String, override val variables: Map[String, Any]) extends AbstractAction(sparkSession, sparkConf, inputVariableKey, outputVariableKey, variables) with Serializable {
     override def doIt(params: Map[String, String]): Any = {
         val rdd = this.pendingData.asInstanceOf[RDD[Map[String, Any]]]
-
         val vectorsRDD: RDD[Tuple1[Vector]] = rdd.map(map => {
-            
             val values = map.get("values").get.asInstanceOf[Array[Double]]
             val vectorLength = map.get("vectorLength").getOrElse(-1).toString().toInt
             if (vectorLength.!=(-1)) {
