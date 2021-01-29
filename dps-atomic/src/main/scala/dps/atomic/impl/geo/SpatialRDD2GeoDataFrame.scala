8bd16368ce281c5dadfc5982c840614305d2f96b
周训游 <zhouxunyou@gmail.com>
2021-01-29 15:57:01 +0800

    fiexed class name

--- dps-atomic/src/main/scala/dps/atomic/impl/geo/SpatialRDD2GeoDataFrame.scala
+++ dps-atomic/src/main/scala/dps/atomic/impl/geo/SpatialRDD2GeoDataFrame.scala
@@ -13,7 +13,7 @@ import org.opengis.referencing.crs.CoordinateReferenceSystem
 import org.geotools.referencing.CRS
 import org.geotools.geometry.jts.JTS
 
-class CoordinateSystemTransformFroSpatialRDD(override val sparkSession: SparkSession, override val sparkConf: SparkConf, override val inputVariableKey: String, override val outputVariableKey: String, override val variables: Map[String, Any]) extends AbstractAction(sparkSession, sparkConf, inputVariableKey, outputVariableKey, variables) with Serializable {
+class SpatialRDD2GeoDataFrame(override val sparkSession: SparkSession, override val sparkConf: SparkConf, override val inputVariableKey: String, override val outputVariableKey: String, override val variables: Map[String, Any]) extends AbstractAction(sparkSession, sparkConf, inputVariableKey, outputVariableKey, variables) with Serializable {
     override def doIt(params: Map[String, String]): Any = {
         val spatialRDD = this.pendingData.asInstanceOf[SpatialRDD[Geometry]]
         val fieldNames = params.get("fieldNames").get.split(",").toList
