b1f8d31c9c33db2070282bf371b9cf2e04322043
周训游 <zhouxunyou@gmail.com>
2021-02-01 11:55:41 +0800

    定义可调用的GeoSparkSQL函数

--- dps-atomic/src/main/scala/dps/atomic/impl/geo/SpatialSQL.scala
+++ dps-atomic/src/main/scala/dps/atomic/impl/geo/SpatialSQL.scala
@@ -6,6 +6,13 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SparkSession
 
 import dps.atomic.impl.AbstractAction
+import dps.atomic.define.AtomOperationDefine
+import dps.atomic.define.AtomOperationParamDefine
+import org.apache.spark.sql.Dataset
+import dps.atomic.define.AtomOperationUdf
+import dps.atomic.define.AtomOperationHasUdfDefine
+import org.apache.spark.sql.Row
+import java.util.concurrent.TimeUnit
 
 class SpatialSQL(override val sparkSession: SparkSession, override val sparkConf: SparkConf, override val inputVariableKey: String, override val outputVariableKey: String, override val variables: Map[String, Any]) extends AbstractAction(sparkSession, sparkConf, inputVariableKey, outputVariableKey, variables) with Serializable {
     override def doIt(params: Map[String, String]): Any = {
@@ -15,5 +22,19 @@ class SpatialSQL(override val sparkSession: SparkSession, override val sparkConf
         val result = sparkSession.sql(spatialSQL)
         result.createOrReplaceTempView(geoViewName)
         variables.put(outputVariableKey, result)
+        
     }
+    override def define(): AtomOperationDefine = {
+        val params = Map(
+            "geoViewName" -> new AtomOperationParamDefine("geo.view.name", "Geometry View Name", true, stringType), //JDBC
+            "spatialSQL" -> new AtomOperationParamDefine("spatialSQL", "select * from dual", true, sqlType))
+        val udfs = Seq(
+            new AtomOperationUdf("ST_Distance", Seq(classOf[String].getName,classOf[String].getName)),
+            new AtomOperationUdf("ST_ConvexHull", Seq(classOf[String].getName)),
+            new AtomOperationUdf("ST_Envelope", Seq(classOf[String].getName)))
+        val template = s"geo/${getClassSimpleName}.ftl"
+        val atomOperation = new AtomOperationHasUdfDefine(getId, getClassName, getClassSimpleName, template, params.toMap, classOf[Nothing], classOf[Dataset[_]], classOf[Nothing], classOf[Row], getTemplateContent(template),udfs)
+        return atomOperation
+    }
+    
 }
