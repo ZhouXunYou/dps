572df2be4c535d1f3c19abe44dc946b64d745846
周训游 <zhouxunyou@gmail.com>
2021-02-24 15:18:56 +0800

    坐标系转换

--- dps-atomic/src/main/scala/dps/atomic/impl/geo/CoordinateSystemTransformFroSpatialRDD.scala
+++ dps-atomic/src/main/scala/dps/atomic/impl/geo/CoordinateSystemTransformFroSpatialRDD.scala
@@ -5,8 +5,6 @@ import scala.collection.mutable.Map
 import org.apache.sedona.core.spatialRDD.SpatialRDD
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
-import org.geotools.geometry.jts.JTS
-import org.geotools.referencing.CRS
 import org.locationtech.jts.geom.Geometry
 
 import dps.atomic.define.AtomOperationDefine
@@ -16,14 +14,11 @@ import dps.atomic.impl.AbstractAction
 class CoordinateSystemTransformFroSpatialRDD(override val sparkSession: SparkSession, override val sparkConf: SparkConf, override val inputVariableKey: String, override val outputVariableKey: String, override val variables: Map[String, Any]) extends AbstractAction(sparkSession, sparkConf, inputVariableKey, outputVariableKey, variables) with Serializable {
   override def doIt(params: Map[String, String]): Any = {
     val spatialRDD = this.pendingData.asInstanceOf[SpatialRDD[Geometry]]
-    val sourceCRS = CRS.decode(params.get("fromEpsgCRSCode").get)
-    val targetCRS = CRS.decode(params.get("targetEpsgCRSCode").get)
+    val sourceCRS = params.get("fromEpsgCRSCode").get
+    val targetCRS = params.get("targetEpsgCRSCode").get
     val isStrict = params.get("strict").get.toBoolean
-    val transform = CRS.findMathTransform(sourceCRS, targetCRS, isStrict)
-    val rdd = spatialRDD.rawSpatialRDD.rdd.map(geom => {
-      JTS.transform(geom, transform)
-    })
-    spatialRDD.rawSpatialRDD = rdd.toJavaRDD()
+    spatialRDD.CRSTransform(sourceCRS, targetCRS, isStrict)
+    variables.put(outputVariableKey, spatialRDD)
   }
 
   override def define: AtomOperationDefine = {
